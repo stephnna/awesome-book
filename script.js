2371221a72760704df78a3162d22b08cd7189649
stephnna <stephennnabuike@gmail.com>
2022-07-02 14:09:33 +0100

    Update index.html style.css script.js

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
+const listLink = document.getElementById('list-link');
+const addNewLink = document.getElementById('add-link');
+const contactLink = document.getElementById('contact-link');
+const addButtonContainer = document.getElementById('add-btn-container');
+const awesomeBookContainer = document.getElementById('list');
+const contact = document.getElementById('contact');
+
+listLink.addEventListener('click', () => {
+  awesomeBookContainer.style.display = 'block';
+  addButtonContainer.style.display = 'none';
+  contact.style.display = 'none';
+});
+
+addNewLink.addEventListener('click', () => {
+  awesomeBookContainer.style.display = 'none';
+  addButtonContainer.style.display = 'block';
+  contact.style.display = 'none';
+});
+
+contactLink.addEventListener('click', () => {
+  awesomeBookContainer.style.display = 'none';
+  addButtonContainer.style.display = 'none';
+  contact.style.display = 'block';
+});
+
 class AwesomeBooks {
   // Initializations
   constructor() {
+    this.dateTime = document.getElementById('date-time');
     this.books = [];
     this.awesomeBooks = document.getElementById('awesome-book');
     this.button = document.getElementById('add-btn');
@@ -10,7 +36,7 @@ class AwesomeBooks {
   displayBooks(book) {
     let bookContainer = '';
     book.forEach((arrayItem, index) => {
-      const bookContent = `<div id=div${index} class="book-container">    
+      const bookContent = `<div id=div${index} class="book-container display-flex">    
     <h4>"${arrayItem.title}" by ${arrayItem.author}</h4>    
     <div><button class="remove-book" id=${index} type="button">Remove</button></div>    
     </div>`;
@@ -22,10 +48,10 @@ class AwesomeBooks {
   // get books in html page if it exist in local storage
   getLocalStorageData() {
     const data = JSON.parse(localStorage.getItem('bookdata'));
-    // console.log(data);
-
+    this.dateTime.innerHTML = Date();
     if (data !== null) {
       this.displayBooks(data);
+
       const bookContent = document.querySelectorAll('.book-container');
       bookContent.forEach((item, index) => {
         if (index % 2 === 0) {
